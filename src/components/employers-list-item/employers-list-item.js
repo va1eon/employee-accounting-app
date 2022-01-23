665fab46d2dc3dd479f9afe44532029870b9f62c
Valery Mikhailov <va1eonmix@gmail.com>
2022-01-23 15:01:00 +0900

    refactor

--- src/components/employers-list-item/employers-list-item.js
+++ src/components/employers-list-item/employers-list-item.js
@@ -4,20 +4,20 @@ const EmployersListItem = (props) => {
   const {name, salary, onDelete, onToggleProp, increase, rise} = props;
 
   let classNames = 'list-group-item d-flex justify-content-between';
-  if(increase) {
+  if (increase) {
     classNames += ' increase';
   }
-  if(rise) {
+  if (rise) {
     classNames += ' like';
   }
 
   return (
     <li className={classNames}>
-        <span
-          className="list-group-item-label"
-          data-toggle="rise"
-          onClick={onToggleProp}
-        >{name}</span>
+      <span
+        className="list-group-item-label"
+        data-toggle="rise"
+        onClick={onToggleProp}
+      >{name}</span>
       <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
       <div className='d-flex justify-content-center align-items-center'>
         <button
@@ -26,16 +26,16 @@ const EmployersListItem = (props) => {
           data-toggle="increase"
           onClick={onToggleProp}
         >
-          <i className="fas fa-cookie" />
+          <i className="fas fa-cookie"/>
         </button>
         <button
           className="btn-trash btn-sm"
           type="button"
           onClick={onDelete}
         >
-          <i className="fas fa-trash" />
+          <i className="fas fa-trash"/>
         </button>
-        <i className="fas fa-star" />
+        <i className="fas fa-star"/>
       </div>
     </li>
   );
