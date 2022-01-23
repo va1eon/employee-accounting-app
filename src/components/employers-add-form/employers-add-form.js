37494c6ae23adf788052d1009e69017e1074aeb2
Valery Mikhailov <va1eonmix@gmail.com>
2022-01-23 20:57:06 +0900

    scss

--- src/components/employers-add-form/employers-add-form.js
+++ src/components/employers-add-form/employers-add-form.js
@@ -1,6 +1,6 @@
 import {Component} from 'react';
-import './employers-add-form.css';
-
+// import './employers-add-form.css';
+import './employers-add-form.scss';
 class EmployersAddForm extends Component {
   constructor(props) {
     super(props);
